bb913a56a60fd76efc48a11c9f04261e02fc0fc8
xsx <29328256+xavierseah@users.noreply.github.com>
2026-09-13 16:38:48 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = "physcards-v1";
-self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(["./", "./index.html", "./sw.js"]))); self.skipWaiting(); });
+const C = "physcards-v2";
+self.addEventListener("install", e => { e.waitUntil(caches.open(C).then(c => c.addAll(["./physics_cards.html", "./sw.js"]))); self.skipWaiting(); });
 self.addEventListener("activate", e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k))))); });
-self.addEventListener("fetch", e => { e.respondWith(fetch(e.request).then(r => { const cp = r.clone(); caches.open(C).then(c => c.put(e.request, cp)); return r; }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))); });
+self.addEventListener("fetch", e => { e.respondWith(fetch(e.request).then(r => { const cp = r.clone(); caches.open(C).then(c => c.put(e.request, cp)); return r; }).catch(() => caches.match(e.request).then(r => r || caches.match("./physics_cards.html")))); });
